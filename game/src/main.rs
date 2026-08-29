b8f45e32df72cfa2163a02b8948b448e6b8aa280
Malo Legendre-Lemaire <malolegendrelemaire@gmail.com>
2026-08-29 11:10:51 +0200

    feat: WGPU Hello World!

--- game/src/main.rs
+++ game/src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use winit::event_loop::EventLoop;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    tracing_subscriber::fmt::init();
+    let event_loop = EventLoop::with_user_event().build()?;
+    let mut engine = engine::Engine::new();
+    event_loop.run_app(&mut engine)?;
+    Ok(())
 }
