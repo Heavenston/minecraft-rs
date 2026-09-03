f4ce4a6178fa868a5186ea84be8a1393e512629f
Malo Legendre-Lemaire <malolegendrelemaire@gmail.com>
2026-09-03 06:41:58 +0200

    fix: Switched to vsync

--- harness/src/renderer.rs
+++ harness/src/renderer.rs
@@ -79,7 +79,7 @@ impl Renderer {
             format: surface_format,
             width: size.width,
             height: size.height,
-            present_mode: surface_caps.present_modes[0],
+            present_mode: wgpu::PresentMode::AutoVsync,
             alpha_mode: surface_caps.alpha_modes[0],
             view_formats: vec![],
             desired_maximum_frame_latency: 2,
