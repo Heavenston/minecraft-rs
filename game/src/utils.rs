965244140d5d669ae282a63cfce16fd0b3c99a42
Malo Legendre-Lemaire <malolegendrelemaire@gmail.com>
2026-09-14 19:15:39 +0200

    feat: Added EightDirection and TwentySixDirection into utils

--- game/src/utils.rs
+++ game/src/utils.rs
@@ -1,7 +1,7 @@
 #![expect(dead_code, reason = "Utils with functions maybe not used")]
 #![expect(unused_imports, reason = "Utils with functions maybe not used")]
 
-use glam::{DVec2, DVec3, ISizeVec3, USizeVec3, Vec2, Vec3};
+use glam::{DVec2, DVec3, I8Vec2, I8Vec3, ISizeVec2, ISizeVec3, USizeVec3, Vec2, Vec3};
 
 pub mod enum_set;
 pub use enum_set::{ EnumSet };
@@ -246,17 +246,17 @@ impl std::fmt::Display for Axis {
 
 #[derive(serde::Deserialize, Debug, Clone, Copy, PartialEq, Eq, Hash, enum_map::Enum)]
 pub enum Sign {
-    Positive,
     Negative,
+    Positive,
 }
 
 impl Sign {
-    pub const VALUES: [Self; 2] = [Self::Positive, Self::Negative];
+    pub const VALUES: [Self; 2] = [Self::Negative, Self::Positive];
 
     pub const fn as_i8(self) -> i8 {
         match self {
-            Self::Positive =>  1,
             Self::Negative => -1,
+            Self::Positive =>  1,
         }
     }
 
@@ -285,8 +285,295 @@ const impl std::ops::Neg for Sign {
     type Output = Self;
     fn neg(self) -> Self::Output {
         match self {
-            Self::Positive => Self::Negative,
             Self::Negative => Self::Positive,
+            Self::Positive => Self::Negative,
+        }
+    }
+}
+
+const impl TryFrom<SignOrZero> for Sign {
+    type Error = ();
+    fn try_from(value: SignOrZero) -> Result<Self, Self::Error> {
+        match value {
+            SignOrZero::Negative => Ok(Self::Negative),
+            SignOrZero::Zero => Err(()),
+            SignOrZero::Positive => Ok(Self::Positive),
+        }
+    }
+}
+
+#[derive(serde::Deserialize, Debug, Clone, Copy, PartialEq, Eq, Hash, enum_map::Enum)]
+pub enum SignOrZero {
+    Negative,
+    Zero,
+    Positive,
+}
+
+impl SignOrZero {
+    pub const VALUES: [Self; 3] = [Self::Negative, Self::Zero, Self::Positive];
+
+    pub const fn as_i8(self) -> i8 {
+        match self {
+            Self::Negative => -1,
+            Self::Zero     =>  0,
+            Self::Positive =>  1,
+        }
+    }
+
+    pub const fn as_isize(self) -> isize {
+        self.as_i8().into()
+    }
+
+    pub const fn as_f32(self) -> f32 {
+        self.as_i8().into()
+    }
+
+    pub const fn as_f64(self) -> f64 {
+        self.as_i8().into()
+    }
+
+    pub const fn is_positive(self) -> bool {
+        matches!(self, Self::Positive)
+    }
+
+    pub const fn is_zero(self) -> bool {
+        matches!(self, Self::Zero)
+    }
+
+    pub const fn is_negative(self) -> bool {
+        matches!(self, Self::Negative)
+    }
+}
+
+const impl std::ops::Neg for SignOrZero {
+    type Output = Self;
+    fn neg(self) -> Self::Output {
+        match self {
+            Self::Positive => Self::Negative,
+            Self::Zero => Self::Zero,
+            Self::Negative => Self::Positive,
+        }
+    }
+}
+
+const impl From<Sign> for SignOrZero {
+    fn from(value: Sign) -> Self {
+        match value {
+            Sign::Positive => Self::Positive,
+            Sign::Negative => Self::Negative,
+        }
+    }
+}
+
+const impl From<SignOrZero> for std::cmp::Ordering {
+    fn from(value: SignOrZero) -> Self {
+        match value {
+            SignOrZero::Positive => Self::Less,
+            SignOrZero::Zero     => Self::Equal,
+            SignOrZero::Negative => Self::Greater,
+        }
+    }
+}
+
+const impl From<std::cmp::Ordering> for SignOrZero {
+    fn from(value: std::cmp::Ordering) -> Self {
+        match value {
+            std::cmp::Ordering::Less    => Self::Positive,
+            std::cmp::Ordering::Equal   => Self::Zero,
+            std::cmp::Ordering::Greater => Self::Negative,
+        }
+    }
+}
+
+#[derive(serde::Deserialize, Debug, Clone, Copy, PartialEq, Eq, Hash, enum_map::Enum)]
+pub enum EightDirection {
+    XN1YN1,
+    XZ0YN1,
+    XP1YN1,
+    XN1YZ0,
+    XP1YZ0,
+    XN1YP1,
+    XZ0YP1,
+    XP1YP1,
+}
+
+impl EightDirection {
+    pub const VALUES: [Self; 8] = [
+        Self::XN1YN1, Self::XZ0YN1, Self::XP1YN1, Self::XN1YZ0,
+        Self::XP1YZ0, Self::XN1YP1, Self::XZ0YP1, Self::XP1YP1,
+    ];
+
+    pub const fn as_signs(self) -> [SignOrZero; 2] {
+        use SignOrZero::{ Negative as N1, Zero as Z0, Positive as P1 };
+        match self {
+            Self::XN1YN1 => [N1,N1], Self::XZ0YN1 => [Z0,N1],
+            Self::XP1YN1 => [P1,N1], Self::XN1YZ0 => [N1,Z0],
+            Self::XP1YZ0 => [P1,Z0], Self::XN1YP1 => [N1,P1],
+            Self::XZ0YP1 => [Z0,P1], Self::XP1YP1 => [P1,P1],
+        }
+    }
+
+    pub const fn opposit(self) -> Self {
+        match self {
+            Self::XN1YN1 => Self::XP1YP1,
+            Self::XZ0YN1 => Self::XZ0YP1,
+            Self::XP1YN1 => Self::XN1YP1,
+            Self::XN1YZ0 => Self::XP1YZ0,
+            Self::XP1YZ0 => Self::XN1YZ0,
+            Self::XN1YP1 => Self::XP1YN1,
+            Self::XZ0YP1 => Self::XZ0YN1,
+            Self::XP1YP1 => Self::XN1YN1,
+        }
+    }
+
+    pub const fn rotate_cw(self) -> Self {
+        match self {
+            Self::XN1YN1 => Self::XN1YZ0,
+            Self::XZ0YN1 => Self::XN1YN1,
+            Self::XP1YN1 => Self::XZ0YN1,
+            Self::XN1YZ0 => Self::XN1YP1,
+            Self::XP1YZ0 => Self::XP1YN1,
+            Self::XN1YP1 => Self::XZ0YP1,
+            Self::XZ0YP1 => Self::XP1YP1,
+            Self::XP1YP1 => Self::XP1YZ0,
+        }
+    }
+
+    pub const fn rotate_ccw(self) -> Self {
+        self.rotate_cw().rotate_cw().rotate_cw().rotate_cw().rotate_cw().rotate_cw().rotate_cw()
+    }
+
+    pub const fn as_i8vec2(self) -> I8Vec2 {
+        let [x, y] = self.as_signs();
+        I8Vec2::new(x.as_i8(), y.as_i8())
+    }
+
+    pub const fn as_isizevec2(self) -> ISizeVec2 {
+        let [x, y] = self.as_signs();
+        ISizeVec2::new(x.as_isize(), y.as_isize())
+    }
+
+    pub const fn as_vec2(self) -> Vec2 {
+        let [x, y] = self.as_signs();
+        Vec2::new(x.as_f32(), y.as_f32())
+    }
+
+    pub const fn as_dvec2(self) -> DVec2 {
+        let [x, y] = self.as_signs();
+        DVec2::new(x.as_f64(), y.as_f64())
+    }
+}
+
+impl enum_set::Enum for EightDirection {
+    type Integer = u8;
+}
+
+#[test]
+fn eight_dir_opposit_test() {
+    for v in EightDirection::VALUES {
+        assert_eq!(v.opposit().opposit(), v);
+        assert_eq!(v.opposit().as_i8vec2(), -v.as_i8vec2());
+    }
+}
+
+
+#[derive(serde::Deserialize, Debug, Clone, Copy, PartialEq, Eq, Hash, enum_map::Enum)]
+pub enum TwentySixDirection {
+    XN1YN1ZN1, XZ0YN1ZN1, XP1YN1ZN1, XN1YZ0ZN1, XZ0YZ0ZN1, XP1YZ0ZN1, XN1YP1ZN1, XZ0YP1ZN1, XP1YP1ZN1,
+    XN1YN1ZZ0, XZ0YN1ZZ0, XP1YN1ZZ0, XN1YZ0ZZ0, XP1YZ0ZZ0, XN1YP1ZZ0, XZ0YP1ZZ0, XP1YP1ZZ0,
+    XN1YN1ZP1, XZ0YN1ZP1, XP1YN1ZP1, XN1YZ0ZP1, XZ0YZ0ZP1, XP1YZ0ZP1, XN1YP1ZP1, XZ0YP1ZP1, XP1YP1ZP1,
+}
+
+impl TwentySixDirection {
+    pub const VALUES: [Self; 26] = [
+        Self::XN1YN1ZN1, Self::XZ0YN1ZN1, Self::XP1YN1ZN1, Self::XN1YZ0ZN1, Self::XZ0YZ0ZN1, Self::XP1YZ0ZN1, Self::XN1YP1ZN1, Self::XZ0YP1ZN1, Self::XP1YP1ZN1,
+        Self::XN1YN1ZZ0, Self::XZ0YN1ZZ0, Self::XP1YN1ZZ0, Self::XN1YZ0ZZ0, Self::XP1YZ0ZZ0, Self::XN1YP1ZZ0, Self::XZ0YP1ZZ0, Self::XP1YP1ZZ0,
+        Self::XN1YN1ZP1, Self::XZ0YN1ZP1, Self::XP1YN1ZP1, Self::XN1YZ0ZP1, Self::XZ0YZ0ZP1, Self::XP1YZ0ZP1, Self::XN1YP1ZP1, Self::XZ0YP1ZP1, Self::XP1YP1ZP1,
+    ];
+
+    pub const fn as_signs(self) -> [SignOrZero; 3] {
+        use SignOrZero::{ Negative as N1, Zero as Z0, Positive as P1 };
+        match self {
+            Self::XN1YN1ZN1 => [N1,N1,N1], Self::XZ0YN1ZN1 => [Z0,N1,N1],
+            Self::XP1YN1ZN1 => [P1,N1,N1], Self::XN1YZ0ZN1 => [N1,Z0,N1],
+            Self::XZ0YZ0ZN1 => [Z0,Z0,N1], Self::XP1YZ0ZN1 => [P1,Z0,N1],
+            Self::XN1YP1ZN1 => [N1,P1,N1], Self::XZ0YP1ZN1 => [Z0,P1,N1],
+            Self::XP1YP1ZN1 => [P1,P1,N1], Self::XN1YN1ZZ0 => [N1,N1,Z0],
+            Self::XZ0YN1ZZ0 => [Z0,N1,Z0], Self::XP1YN1ZZ0 => [P1,N1,Z0],
+            Self::XN1YZ0ZZ0 => [N1,Z0,Z0], Self::XP1YZ0ZZ0 => [P1,Z0,Z0],
+            Self::XN1YP1ZZ0 => [N1,P1,Z0], Self::XZ0YP1ZZ0 => [Z0,P1,Z0],
+            Self::XP1YP1ZZ0 => [P1,P1,Z0], Self::XN1YN1ZP1 => [N1,N1,P1],
+            Self::XZ0YN1ZP1 => [Z0,N1,P1], Self::XP1YN1ZP1 => [P1,N1,P1],
+            Self::XN1YZ0ZP1 => [N1,Z0,P1], Self::XZ0YZ0ZP1 => [Z0,Z0,P1],
+            Self::XP1YZ0ZP1 => [P1,Z0,P1], Self::XN1YP1ZP1 => [N1,P1,P1],
+            Self::XZ0YP1ZP1 => [Z0,P1,P1], Self::XP1YP1ZP1 => [P1,P1,P1],
+        }
+    }
+
+    pub const fn opposit(self) -> Self {
+        match self {
+            Self::XN1YN1ZN1 => Self::XP1YP1ZP1, Self::XZ0YN1ZN1 => Self::XZ0YP1ZP1,
+            Self::XP1YN1ZN1 => Self::XN1YP1ZP1, Self::XN1YZ0ZN1 => Self::XP1YZ0ZP1,
+            Self::XZ0YZ0ZN1 => Self::XZ0YZ0ZP1, Self::XP1YZ0ZN1 => Self::XN1YZ0ZP1,
+            Self::XN1YP1ZN1 => Self::XP1YN1ZP1, Self::XZ0YP1ZN1 => Self::XZ0YN1ZP1,
+            Self::XP1YP1ZN1 => Self::XN1YN1ZP1, Self::XN1YN1ZZ0 => Self::XP1YP1ZZ0,
+            Self::XZ0YN1ZZ0 => Self::XZ0YP1ZZ0, Self::XP1YN1ZZ0 => Self::XN1YP1ZZ0,
+            Self::XN1YZ0ZZ0 => Self::XP1YZ0ZZ0, Self::XP1YZ0ZZ0 => Self::XN1YZ0ZZ0,
+            Self::XN1YP1ZZ0 => Self::XP1YN1ZZ0, Self::XZ0YP1ZZ0 => Self::XZ0YN1ZZ0,
+            Self::XP1YP1ZZ0 => Self::XN1YN1ZZ0, Self::XN1YN1ZP1 => Self::XP1YP1ZN1,
+            Self::XZ0YN1ZP1 => Self::XZ0YP1ZN1, Self::XP1YN1ZP1 => Self::XN1YP1ZN1,
+            Self::XN1YZ0ZP1 => Self::XP1YZ0ZN1, Self::XZ0YZ0ZP1 => Self::XZ0YZ0ZN1,
+            Self::XP1YZ0ZP1 => Self::XN1YZ0ZN1, Self::XN1YP1ZP1 => Self::XP1YN1ZN1,
+            Self::XZ0YP1ZP1 => Self::XZ0YN1ZN1, Self::XP1YP1ZP1 => Self::XN1YN1ZN1,
+        }
+    }
+
+    pub const fn as_i8vec3(self) -> I8Vec3 {
+        let [x, y, z] = self.as_signs();
+        I8Vec3::new(x.as_i8(), y.as_i8(), z.as_i8())
+    }
+
+    pub const fn as_isizevec3(self) -> ISizeVec3 {
+        let [x, y, z] = self.as_signs();
+        ISizeVec3::new(x.as_isize(), y.as_isize(), z.as_isize())
+    }
+
+    pub const fn as_vec3(self) -> Vec3 {
+        let [x, y, z] = self.as_signs();
+        Vec3::new(x.as_f32(), y.as_f32(), z.as_f32())
+    }
+
+    pub const fn as_dvec3(self) -> DVec3 {
+        let [x, y, z] = self.as_signs();
+        DVec3::new(x.as_f64(), y.as_f64(), z.as_f64())
+    }
+}
+
+impl From<CardinalDirection> for TwentySixDirection {
+    fn from(value: CardinalDirection) -> Self {
+        match value {
+            CardinalDirection::PosX => Self::XP1YZ0ZZ0,
+            CardinalDirection::NegX => Self::XN1YZ0ZZ0,
+            CardinalDirection::PosY => Self::XZ0YP1ZZ0,
+            CardinalDirection::NegY => Self::XZ0YN1ZZ0,
+            CardinalDirection::PosZ => Self::XZ0YZ0ZP1,
+            CardinalDirection::NegZ => Self::XZ0YZ0ZN1,
+        }
+    }
+}
+
+impl enum_set::Enum for TwentySixDirection {
+    type Integer = u8;
+}
+
+const impl std::ops::Add<TwentySixDirection> for ISizeVec3 {
+    type Output = Self;
+    fn add(self, rhs: TwentySixDirection) -> Self::Output {
+        if cfg!(debug_assertions) {
+            self.checked_add(rhs.as_isizevec3()).expect("arithetic overflow")
+        }
+        else {
+            self.wrapping_add(rhs.as_isizevec3())
         }
     }
 }
